c13d48c9254e2a0576e6b6a98268489b80b96b53
eiei114 <nowaka37564@gmail.com>
2023-02-26 14:01:07 +0900

    fix 修正

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ results = notion.databases.query(
         "database_id": database_id,
         "filter": {
             "property": "Status",
-            "select": {
+            "status": {
                 "equals": "Done"
             }
         }
@@ -22,10 +22,19 @@ results = notion.databases.query(
 
 now = datetime.datetime.now(datetime.timezone.utc)
 
-for result in results:
-    page_id = result["id"]
-    last_edited_time = datetime.datetime.fromisoformat(result["last_edited_time"][:-1] + '+00:00')
-    delta = now - last_edited_time
+if not results:
+    print("There are no results to update.")
+else:
+    for result in results:
+        page_id = result["id"]
+        last_edited_time = datetime.datetime.fromisoformat(result["last_edited_time"][:-1] + '+00:00')
+        delta = now - last_edited_time
 
-    if delta.days >= 3:
-        notion.pages.update(page_id=page_id, properties={"Status": {"name": "Archive"}})
+        if delta.days >= 3:
+            updated_props = {
+                "Status": {
+                    "name": "Archive"
+                }
+            }
+            notion.pages.update(page_id=page_id, properties=updated_props)
+            print(f"Page {page_id} has been updated.")
